d6ab301123277f34f2c6d6cc5f24611326f95679
choijonghak123 <111619182+choijonghak123@users.noreply.github.com>
2025-02-13 12:28:49 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -8,10 +8,8 @@ import os
 from dotenv import load_dotenv
 ## load the GROQ API Key
 load_dotenv()
+api_key=st.secrets['GROQ_API_KEY']
 
-os.environ['GROQ_API_KEY']=os.getenv("GROQ_API_KEY")
-
-api_key=os.getenv("GROQ_API_KEY")
 ## Arxiv and wikipedia Tools
 arxiv_wrapper=ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=200)
 arxiv=ArxivQueryRun(api_wrapper=arxiv_wrapper)
